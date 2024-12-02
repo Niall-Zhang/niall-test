8314686961b5520f496d5a701e5652fcc983fc7b
Niall-Zhang <niall.zhang.hc@gmail.com>
2024-12-01 23:50:51 -0800

    Initial commit for niall-test project

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-blue-500 text-white">
+        <h1 className="text-4xl font-bold mb-4">About Me</h1>
+        <p className="text-lg">
+          Hi, I'm Niall! A software engineer passionate about building modern web applications.
+        </p>
+      </div>
+    )
+  }
